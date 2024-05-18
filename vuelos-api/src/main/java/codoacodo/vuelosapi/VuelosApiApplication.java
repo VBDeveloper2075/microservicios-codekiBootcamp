fb024b4284ff451bc285dfa32f0bd11c0a2ca66d
Veronica <dv.veronica2075@gmail.com>
2024-05-18 18:53:08 -0300

    conexión eureka / api-vuelo

--- vuelos-api/src/main/java/codoacodo/vuelosapi/VuelosApiApplication.java
+++ vuelos-api/src/main/java/codoacodo/vuelosapi/VuelosApiApplication.java
@@ -3,9 +3,11 @@ package codoacodo.vuelosapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class VuelosApiApplication {
 
     public static void main(String[] args) {
